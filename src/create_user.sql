323665318af3aeb123a7bf6dff1dd07762e4f40d
David Pyke Le Brun <shoelace_822695@hotmail.com>
2014-05-02 09:14:31 +0100

    add public synonyms
    mend

--- src/create_user.sql
+++ src/create_user.sql
@@ -3,15 +3,21 @@ CREATE USER log4 IDENTIFIED BY log4;
 GRANT CONNECT, RESOURCE TO log4;
 GRANT UNLIMITED TABLESPACE TO log4;
 
---needed for AQ impl
-GRANT aq_administrator_role TO log4;
-GRANT EXECUTE ON dbms_aq TO log4;
-GRANT EXECUTE ON dbms_aqadm TO log4;
 
 grant create procedure to log4;
 grant create sequence to log4;
 grant create table to log4;
 grant create type to log4;
 grant create view to log4;
--- optional
+
+-- optional grants
 grant SELECT ANY DICTIONARY to log4;
+
+--
+grant create synonym to log4;
+grant create public synonym to log4;
+
+--needed for AQ impl. not current used
+--GRANT aq_administrator_role TO log4;
+--GRANT EXECUTE ON dbms_aq TO log4;
+--GRANT EXECUTE ON dbms_aqadm TO log4;
--- src/grants.sql
+++ src/grants.sql
@@ -5,7 +5,7 @@ grant execute on Marker to public;
 grant execute on LogLevel to public;
 
 --tables
-grant select  on log_table to public;
+grant select,delete  on log_table to public;
 grant select,insert,update,delete on log_levels to public;
 
 --packages
@@ -14,11 +14,11 @@ grant execute on MarkerManager to public;
 grant execute on ThreadContext to public;
 
 
-create public synonym Logger for Logger;
-create public synonym LogManager for LogManager;
-create public synonym MarkerManager for MarkerManager;
-create public synonym ThreadContext for ThreadContext;
+create public synonym Logger for log4.Logger;
+create public synonym LogManager for log4.LogManager;
+create public synonym MarkerManager for log4.MarkerManager;
+create public synonym ThreadContext for log4.ThreadContext;
 
-create public synonym log_table for log_table;
-create public synonym log_levels for log_levels;
+create public synonym log_table for log4.log_table;
+create public synonym log_levels for log4.log_levels;
 
