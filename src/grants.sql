4f04f60bfd9b3ef6f82128c458e7ca192e997b31
David Pyke Le Brun <shoelace_822695@hotmail.com>
2014-05-12 11:35:18 +0100

    add additional grants/synonyms

--- src/grants.sql
+++ src/grants.sql
@@ -1,8 +1,13 @@
 --types
 grant execute on log4_object to public;
 grant execute on Logger to public;
+grant execute on Filter to public;
 grant execute on Marker to public;
 grant execute on LogLevel to public;
+grant execute on GenericException to public;
+grant execute on DBMSOutputAppender to public;
+grant execute on Layout to public;
+grant execute on PatternLayout to public;
 
 --tables
 grant select,delete  on log_table to public;
@@ -16,10 +21,17 @@ grant execute on ThreadContext to public;
 
 create public synonym Logger for log4.Logger;
 create public synonym LogLevel for log4.Logger;
+create public synonym GenericException for log4.GenericException;
+
 create public synonym LogManager for log4.LogManager;
 create public synonym MarkerManager for log4.MarkerManager;
 create public synonym ThreadContext for log4.ThreadContext;
 
 create public synonym log_table for log4.log_table;
 create public synonym log_levels for log4.log_levels;
+
+create public synonym DBMSOutputAppender for log4.DBMSOutputAppender ;
+create public synonym Layout for log4.Layout ;
+create public synonym PatternLayout for log4.PatternLayout ;
+
 
